aabdce2b28aa54687e7151a2a169b2f83445cea1
Binayak Ghosh <ghoshbinayak@gmail.com>
2015-03-12 11:22:10 +0530

    finally started to (copy) code!

---
+++ pass.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+#include "hashtab.h"
+
+int main(int argc, char const *argv[])
+{
+	hashtable_t *hashtable = ht_create( 65536 );
+
+	ht_set( hashtable, "key1", "inky" );
+	ht_set( hashtable, "key2", "pinky" );
+	ht_set( hashtable, "key3", "blinky" );
+	ht_set( hashtable, "key4", "floyd" );
+
+	printf( "%s\n", ht_get( hashtable, "key1" ) );
+	printf( "%s\n", ht_get( hashtable, "key2" ) );
+	printf( "%s\n", ht_get( hashtable, "key3" ) );
+	printf( "%s\n", ht_get( hashtable, "key4" ) );
+return 0;
+}
