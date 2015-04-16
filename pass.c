29fbea9243d6eef3e244194e8f73f4d1f684f37e
Binayak Ghosh <ghoshbinayak@gmail.com>
2015-04-16 09:32:18 +0530

    started work on main code (finally!). trying to read from a file and populate the tables

--- pass.c
+++ pass.c
@@ -1,8 +1,31 @@
 #include "stdio.h"
 #include "hashtab.h"
 
+// function prototypes
+int pass1();
+
+// Global variables
+FILE * ifp;
+
 int main(int argc, char const *argv[])
 {
+	// Read the arguments
+	if (argc < 2)
+	{
+		printf("%s\n", "usage: pass <file name>");
+		exit(0);
+	}
+
+	// Try to open the file
+	ifp = fopen(argv[1], "r");
+	if (ifp == NULL)
+	{
+		printf("%s\n", "error: file not found");
+		exit(0);
+	}
+
+	pass1();
+	// hash table declaration
 	hashtable_t *hashtable = ht_create( 65536 );
 
 	ht_set( hashtable, "key1", "inky" );
@@ -14,5 +37,16 @@ int main(int argc, char const *argv[])
 	printf( "%s\n", ht_get( hashtable, "key2" ) );
 	printf( "%s\n", ht_get( hashtable, "key3" ) );
 	printf( "%s\n", ht_get( hashtable, "key4" ) );
-return 0;
+
+	// 
+	return 0;
+}
+
+int pass1(){
+	char c;
+	while ((c = getc(ifp)) != EOF){
+
+	}
+
+	return 1;
 }
